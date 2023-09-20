fca2067e1294d9532dd16a998bdf5494748bd28c
René Janssen <rjanssen@barracuda.com>
2023-09-20 11:33:22 +0200

    add extra html removal step, sort mime_type processing logic out

--- src/mail_classifier/clean_emails.py
+++ src/mail_classifier/clean_emails.py
@@ -3,6 +3,7 @@ import email
 import json
 import logging
 import time
+import re
 from bs4 import BeautifulSoup
 from email import policy
 from pathlib import Path
@@ -10,6 +11,8 @@ from pathlib import Path
 loglevel = logging.DEBUG
 logging.basicConfig(level=loglevel)
 log = logging.getLogger(__name__)
+
+html_regex = re.compile("<.*?>")
 
 
 def clean_string(raw_string):
@@ -26,7 +29,48 @@ def html_to_string(mailpart):
     return textstring
 
 
-def mail_to_dict(mails):
+def remove_html_tags(text: str) -> str:
+    return " ".join(re.sub(html_regex, "", text).split())
+
+
+def mail_to_dict_reduced(mails):
+    mail_dicts = {}
+    for mail_tuple in mails:
+        subject = mail_tuple[1].get("Subject")
+        maildate = mail_tuple[1].get("Date")
+        isotimestamp = email.utils.parsedate_to_datetime(maildate).isoformat()
+        mime_parts = [part.get_content_type() for part in mail_tuple[1].walk()]
+        texts = []
+        use_html = False
+        for part in mail_tuple[1].walk():
+            if part.get_content_subtype() == "alternative":
+                continue
+            if part.get_content_subtype() == "plain":
+                plain_clean = clean_string(str(part.get_payload(decode=False)))
+                if not plain_clean:
+                    use_html = True
+                else:
+                    part_text = plain_clean
+            if part.get_content_subtype() == "html" and "text/plain" in mime_parts:
+                continue
+            if part.get_content_subtype() == "html" and "text/plain" not in mime_parts:
+                part_text = html_to_string(part)
+            if use_html and part.get_content_subtype() == "html":
+                part_text = html_to_string(part)
+            cleaner_text = remove_html_tags(part_text)
+            texts.append(cleaner_text)
+
+        mail_dict = {
+            "Received_date": isotimestamp,
+            "Subject": subject,
+            "payload_texts": texts,
+            "label": mail_tuple[2],
+        }
+        mail_dicts[mail_tuple[0]] = mail_dict
+    return mail_dicts
+
+
+def mail_to_dict_full(mails):
     mail_dicts = {}
     for mail_tuple in mails:
         subject = mail_tuple[1].get("Subject")
@@ -90,10 +134,10 @@ def main():
     log.info("%d emails removed", len(all_mails) - len(mails))
 
     log.info("Processing %d emails", len(mails))
-    processed_mails = mail_to_dict(mails)
+    processed_mails = mail_to_dict_reduced(mails)
 
     log.info("writing json to %s", mail_dir)
-    with open(Path(mail_dir, "cleaned_emails_script.json"), "w", encoding="utf-8") as f:
+    with open(Path(mail_dir, "cleaned_good_emails.json"), "w", encoding="utf-8") as f:
         json.dump(processed_mails, f, ensure_ascii=False, indent=4)
     end_time = time.monotonic()
     log.info("Email processing done, elapsed time %ss", round(end_time - start_time, 1))
